028e4bc05edf40152807e451a03f90dec14fef53
amatis-herson <45370521+amatis-herson@users.noreply.github.com>
2019-11-27 01:09:01 +0400

    commit

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 tutorial = require('./tutorial');
 console.log(tutorial.sum(4,5));
+console.log(tutorial.substract(4,5));
+console.log(tutorial.multiply(4,5));
+console.log(tutorial.division(4,5));
--- tutorial.js
+++ tutorial.js
@@ -1,4 +1,8 @@
 console.log("test1");
 const sum = (x, y) => x + y;
+const substract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const division = (x, y) => x / y;
 
-module.exports.sum = sum
+//module.exports.sum = sum
+module.exports = {sum : sum, substract : substract, multiply : multiply, division : division}
